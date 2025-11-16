3b472a6718f66683725861503588fa41aed2d283
Diego Parrales <diegoparrales2005@gmail.com>
2025-11-16 10:01:16 -0500

    primera prueba de videollamada

--- src/server.js
+++ src/server.js
@@ -1,11 +1,39 @@
 import 'dotenv/config';
 import app from './app.js';
-import { testConnection } from '@config/db.js';
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app); 
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("🟢 Usuario conectado:", socket.id);
+
+  socket.on("offer", (data) => {
+    socket.broadcast.emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.broadcast.emit("answer", data);
+  });
+
+  socket.on("ice-candidate", (data) => {
+    socket.broadcast.emit("ice-candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔴 Usuario desconectado:", socket.id);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
-testConnection().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-  });
+httpServer.listen(PORT, () => {
+  console.log(`🚀 API + Socket.io corriendo en http://localhost:${PORT}`);
 });
